411a68b1e74fa088d5dcf2d8a3c076b84c820587
Bryan Alba <bryanalba92@gmail.com>
2022-05-11 23:46:29 -0500

    useNavigate

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -1,23 +1,15 @@
 import React, { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../auth/AuthContext';
 import { types } from '../../types/types';
 
 export const LoginScreen = ({history}) => {
 
-    const {dispatch} = useContext(AuthContext);
+    const navigate = useNavigate();
+
 
     const handleLogin = () => {
-
-
-        const lastPath = localStorage.getItem('lastPath') || '/';
-
-        dispatch({
-            type: types.login,
-            payload: {
-                name: 'Andres',
-            }
-        })
-        history.replace(lastPath);
+        navigate('/marvel', { replace: true })
     }
 
     return (
